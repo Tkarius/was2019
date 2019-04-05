f67a2126e371f408dc79c217b3ea0b413d02d926
Jussi <jussi.kungas@hotmail.com>
2019-04-06 01:28:51 +0300

    Create sivu

--- frontend/create.php
+++ frontend/create.php
@@ -6,5 +6,17 @@
     </head>
     <body>
         <?php require ('menu_bar.php'); ?>
+
+        <div class="contentarea">
+        <h2>Create a Business Announcement</h2>
+            <form method="POST" action="/wassivu">
+                <div class='bnsannouncement'>
+                <div class='firstline'><span class='title'> <input type="text" name="title" placeholder="Title"> </span> - <span class='date'> <input type="date" name="date"> </span></div>
+                <span class='category'><input type="text" name="category" placeholder="Category"></span>
+                <div class='description'><textarea name="description">Description</textarea></div>
+                </div>
+                <input type="submit" value="Create">
+            </form>
+        </div>
     </body>
 </html>
