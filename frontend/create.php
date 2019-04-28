32d49d36a9742594f10cd227fc79768efaef55b0
Jussi <jussi.kungas@hotmail.com>
2019-04-28 22:38:25 +0300

    Lisätty interval, mikä tallentaa epoch ajan input hiddeniin.

--- frontend/create.php
+++ frontend/create.php
@@ -11,7 +11,7 @@
         <h2>Create a Business Announcement</h2>
             <form method="POST" action="/wassivu">
                 <div class='bnsannouncement'>
-                <div class='firstline'><span class='title'> <input type="text" name="title" placeholder="Title" required> </span> - <span class='date'> <input type="date" name="date" required> - <input type="email" name="email" placeholder="email" required> - <input type="text" name="fullname" placeholder="Full name" required></div>
+                <div class='firstline'><span class='title'> <input type="text" name="title" placeholder="Title" required> </span> - <span class='date'> <input id="date_id" type="date" required> - <input type="email" name="email" placeholder="email" required> - <input type="text" name="fullname" placeholder="Full name" required></div>
                 <span class='category'>
                 <select name="category">
                     <option value="buying">Buying</option>
@@ -20,8 +20,19 @@
                 </span>
                 <div class='description'><textarea name="description">Description</textarea></div>
                 </div>
+                <input id="hidden_date_epoch" name="date" type="hidden">
                 <input type="submit" value="Create">
             </form>
         </div>
     </body>
+
+    <script type="text/javascript">
+
+        setInterval(function(){  
+            var Epochdate = new Date(document.getElementById("date_id").value);
+            Epochdate = Epochdate.getTime();
+            document.getElementById("hidden_date_epoch").value = Epochdate;
+        }, 10);
+
+    </script>
 </html>
