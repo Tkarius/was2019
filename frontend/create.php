0814ccbb0a8e3175306633a5596ee3d2fb2f0be8
Jussi <jussi.kungas@hotmail.com>
2019-04-28 18:05:22 +0300

    Korjauksia

--- frontend/create.php
+++ frontend/create.php
@@ -11,8 +11,13 @@
         <h2>Create a Business Announcement</h2>
             <form method="POST" action="/wassivu">
                 <div class='bnsannouncement'>
-                <div class='firstline'><span class='title'> <input type="text" name="title" placeholder="Title"> </span> - <span class='date'> <input type="date" name="date"> - <span class='date'> <input type="email" name="email" placeholder="email"> </span></div>
-                <span class='category'><input type="text" name="category" placeholder="Category"></span>
+                <div class='firstline'><span class='title'> <input type="text" name="title" placeholder="Title" required> </span> - <span class='date'> <input type="date" name="date" required> - <input type="email" name="email" placeholder="email" required> - <input type="text" name="fullname" placeholder="Full name" required></div>
+                <span class='category'>
+                <select name="category">
+                    <option value="buying">Buying</option>
+                    <option value="selling">Selling</option>
+                </select>
+                </span>
                 <div class='description'><textarea name="description">Description</textarea></div>
                 </div>
                 <input type="submit" value="Create">
