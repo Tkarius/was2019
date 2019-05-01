1907e0a325a7e660e2c61e0e88d090b845e24340
Mira Pohjola <mira.pohjola89@gmail.com>
2019-05-01 14:59:44 +0300

    Added http method filtering to only allow GET and POST requests through the proxy.
    Also random prints added to server routes for testing.

--- backend/handlers.go
+++ backend/handlers.go
@@ -1,11 +1,13 @@
 package main
 
 import "net/http"
+import "fmt"
 
 func listAnnouncements(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("Go Esteri, Go!")
 
 }
 
 func createAnnouncement(w http.ResponseWriter, r *http.Request) {
-
+    fmt.Println("Go Esteri, Go!")
 }
